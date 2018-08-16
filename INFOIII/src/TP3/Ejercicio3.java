b443c27933655e85ad98242a829589f4b50ee66f
Igna <igutierrez351@alumnos.iua.edu.ar>
2018-08-16 08:53:24 -0300

    Ejercicio 3 resuelto

---
+++ INFOIII/src/TP3/Ejercicio3.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3;
+
+/**
+ *
+ * @author nacho
+ */
+public class Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] info = {3,5,7,9,11,13,15};
+        mostrar_matriz(info, info.length);
+    }
+    
+    public static void mostrar_matriz(int[] datos, int tam){
+        if(tam != 0){
+        
+        System.out.println(datos[tam-1]);
+        
+        mostrar_matriz(datos, tam-1);
+        }
+        /*if(tam == 0)
+            return;
+        */
+           
+    }
+        
+}
